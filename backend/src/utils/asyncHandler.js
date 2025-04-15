54568cbe7d99c23a0453444013168fdc77085692
Yash Varma <yash.v220104@gmail.com>
2025-04-15 17:14:54 +0530

    utils setup

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+
+const asyncHandler = (fn) => {
+    (req, res, next) => {
+        Promise
+            .resolve(fn(req, res, next))
+            .catch((err) => next(err))
+    }
+}
+
+
+// Using async, try & catch!
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (err) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+
+export { asyncHandler }
