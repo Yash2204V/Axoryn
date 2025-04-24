09b5d55b585a22995dcf7faa2be5f834b652d9a7
Yash Varma <yash.v220104@gmail.com>
2025-04-24 15:21:23 +0530

    Enhance user routes: add account update, avatar, cover image, and user profile retrieval functionalities

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserCurrentProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -23,6 +23,22 @@ router.route("/login").post(loginUser);
 
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/avatar").patch(verifyJWT, 
+    upload.single("avatar"), 
+    updateUserAvatar
+);
+router.route("/cover-image").patch(verifyJWT, 
+    upload.single("coverImage"), 
+    updateUserCoverImage
+);
+
+router.route("/c/:username").get(verifyJWT, getUserCurrentProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
+
+
 export default router
