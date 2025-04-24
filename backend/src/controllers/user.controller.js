8dac6f06d58d7d9ab0d77d218ce9d402ca7a5536
Yash Varma <yash.v220104@gmail.com>
2025-04-24 15:11:36 +0530

    Add user profile and watch history retrieval features

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 
 const generateAccessAndRefreshToken = async (userId) => {
@@ -221,7 +222,7 @@ const getCurrentUser = asyncHandler(async (req, res) => {
 
     return res
         .status(200)
-        .json(200, req.user, "Current User Fetched Successfully")
+        .json(new ApiResponse(200, req.user, "Current User Fetched Successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -269,24 +270,24 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
         new: true
     }).select("-password")
 
-    
+
     return res
-    .status(200)
-    .json(
-        new ApiResponse(200, user, "Avatar Uploaded Successfully")
-    )
+        .status(200)
+        .json(
+            new ApiResponse(200, user, "Avatar Uploaded Successfully")
+        )
 })
 
 const updateUserCoverImage = asyncHandler(async (req, res) => {
     const coverImagePath = req.file?.path;
 
-    if(!coverImagePath) {
+    if (!coverImagePath) {
         throw new ApiError(400, "Cover Image is missing");
     }
 
     const coverImage = uploadOnCloudinary(coverImagePath);
 
-    if(!coverImage.url) {
+    if (!coverImage.url) {
         throw new ApiError(400, "Error while uploading Cover Image");
     }
 
@@ -299,12 +300,139 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     }).select("-password")
 
     return res
-    .status(200)
-    .json(
-        new ApiResponse(200, user, "Cover Image Uploaded Successfully")
-    )
+        .status(200)
+        .json(
+            new ApiResponse(200, user, "Cover Image Uploaded Successfully")
+        )
 
 })
+
+const getUserCurrentProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "Username is missing");
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions", // from where?
+                localField: "_id", // In the users collection
+                foreignField: "channel", // In the subscriptions collection
+                as: "subscribers" // New field: subscribers in db. 
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo" // New field: subscribedTo in db. 
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: { // New field: subscribersCount in db.
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: { // New field: channelsSubscribedToCount in db.
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: { // New field: isSubscribed in db.
+                    $condition: {
+                        if: { $in: [req.user?._id, "subscribers.subscriber"] }, // $in: It is see within array & objects as well.
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: { // Final projection: What fields do you like?
+                fullname: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ]);
+
+    // console.log(channel);
+    if (!channel?.length) {
+        throw new ApiError(404, "Channel does not exists.")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, "Channel Successfully Fetched")
+        )
+
+})
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    // console.log(channel);
+    if (!user?.length) {
+        throw new ApiError(404, "Watch History Fetching Failed")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, user[0].watchHistory, "Watch History Fetched Successfully"))
+})
+
 
 export {
     registerUser,
@@ -315,5 +443,7 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserCurrentProfile,
+    getWatchHistory
 }
